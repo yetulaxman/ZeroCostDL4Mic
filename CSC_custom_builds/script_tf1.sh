a649920f454c17c2b9e572b4727f7c17fb3c2de3
Laxman <48151266+yetulaxman@users.noreply.github.com>
2023-01-22 22:19:24 +0200

    Update script_tf1.sh

--- CSC_custom_builds/script_tf1.sh
+++ CSC_custom_builds/script_tf1.sh
@@ -1,6 +1,7 @@
 # remove all installations from home: rm -fr ~/.local/*
 module load purge
 module load tykky
+# mkdir /projappl/project_xxx/user_name
 mkdir /projappl/project_xxx/user_name/ZeroCostDL4Mic_convert
 conda-containerize new --prefix  /projappl/project_xxx/user_name/ZeroCostDL4Mic_convert  stardist_python3_7.yml
 export PATH="/projappl/project_xxx/user_name/ZeroCostDL4Mic_convert/bin:$PATH"
